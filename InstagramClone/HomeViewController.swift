bfaed38d5001055f671f861401f3b96d82e2e99d
Samantha Wiley <samantha.wiley@student.csulb.edu>
2019-01-16 13:57:53 -0800

    Instagram Tab Bar

---
+++ InstagramClone/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  InstagramClone
+//
+//  Created by Samantha Wiley on 1/16/19.
+//  Copyright © 2019 Samantha Wiley. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // change color of view background
+        view.backgroundColor = UIColor.black
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstagramClone/ProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  InstagramClone
+//
+//  Created by Samantha Wiley on 1/16/19.
+//  Copyright © 2019 Samantha Wiley. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // change color of view background
+        view.backgroundColor = UIColor.brown
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
